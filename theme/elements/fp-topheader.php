2f29975154ca204934cb324827c225f3b87bb7b4
Kazuki Fukuda <nisshin@dtdsh.com>
2018-03-13 14:24:22 +0900

    「適切な賠償を受けるには」に変更

--- theme/elements/fp-topheader.php
+++ theme/elements/fp-topheader.php
@@ -7,11 +7,11 @@
 	);
 	$slider = new WP_Query( $slides_args );
 ?>
-<section class="fp--topheader">
-	<div class="row align-middle">
-		<div class="column small-12 medium-6 slider--wrapper">
-			<div id="js--top-slider" class="slick-slider">
-			<?php
+	<section class="fp--topheader">
+		<div class="row align-middle">
+			<div class="column small-12 medium-6 slider--wrapper">
+				<div id="js--top-slider" class="slick-slider">
+					<?php
 				if ( $slider->have_posts() ) :
 					while ( $slider->have_posts() ) :
 						$slider->the_post();
@@ -23,36 +23,64 @@
 							$link_src = get_post_meta( $id, 'slider_link', true );
 							$link_title = ( get_post_meta( $id, 'slider_link_title', true ) ) ? get_post_meta( $id, 'slider_link_title', true ) : get_the_title();
 				?>
-				<article class="slick-slide slide--item card">
-					<div class="card--img"><img src="<?php echo $img; ?>" alt="<?php the_title(); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>"></div>
-					<div class="card--content">
-						<p class="card--title"><?php the_title(); ?></p>
-						<p><?php
+						<article class="slick-slide slide--item card">
+							<div class="card--img">
+								<img src="<?php echo $img; ?>" alt="<?php the_title(); ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
+							</div>
+							<div class="card--content">
+								<p class="card--title">
+									<?php the_title(); ?>
+								</p>
+								<p>
+									<?php
 							if ( '' !== $post->post_content ) {
 								the_content();
 							} else {
 								echo "<a href=\"$link_src\" title=\"$link_title\" class=\"button\">$link_title</a>";
 							}
-						?></p>
-					</div>
-				</article>
-				<?php
+						?>
+								</p>
+							</div>
+						</article>
+						<?php
 						endwhile;
 					endif;
 				?>
-				<div id="slide--button"></div>
+						<div id="slide--button"></div>
+				</div>
+			</div>
+			<div class="column small-12 medium-6 copy--top text-center">
+				<h1 class="copy--title">交通事故で適切な慰謝料などを得るために、
+					<br>探しているのは頼れる
+					<ruby>
+						<rb>弁護士</rb>
+						<rp>
+							<rt>みかた</rt>
+						</rp>
+					</ruby>。</h1>
+				<div class="copy--desc">
+					<p>相談件数17,000件以上。
+						<small>※</small>
+						<br>選ぶなら広島最大級。</p>
+					<small>※　平成29年12月時点</small>
+				</div>
+				<p class="topheader--button">
+					<a href="tel:0120783409" title="電話する" class="button hollow expanded">
+						<img src="//www.law-yamashita.com/wp-content/themes/ym-home/assets/img/header-tel.jpg" alt="">
+					</a>
+				</p>
+				<p class="topheader--button">
+					<a href="https://www.law-yamashita.com/contact" class="button hollow expanded" title="メールで相談予約" target="_blank">
+						<?php NID_SVG::icon( 'mail', array( 'class' => 'button--icon' ), 'メール' ); ?>メールで相談予約</a>
+				</p>
+				<p class="topheader--button button-group expanded stacked-for-small">
+					<a href="#front--flow" class="button hollow expanded" title="解決の流れ">解決の流れ</a>
+					<a href="#front--cases" class="button hollow expanded" title="こんなに違う！慰謝料などの賠償金">適切な賠償を受けるには</a>
+				</p>
+				<p class="topheader--button">
+					<a href="<?php echo home_url(), '/firm/access'; ?>" class="button hollow expanded" title="山下江法律事務所へのアクセス">
+						<?php NID_SVG::icon( 'map', array( 'class' => 'button--icon' ), 'アクセス' ); ?>アクセス</a>
+				</p>
 			</div>
 		</div>
-		<div class="column small-12 medium-6 copy--top text-center">
-			<h1 class="copy--title">交通事故で適切な慰謝料などを得るために、<br>探しているのは頼れる<ruby><rb>弁護士</rb><rp><rt>みかた</rt></rp></ruby>。</h1>
-			<div class="copy--desc">
-				<p>相談件数17,000件以上。<small>※</small><br>選ぶなら広島最大級。</p>
-			<small>※　平成29年12月時点</small>
-			</div>
-			<p class="topheader--button"><a href="tel:0120783409" title="電話する" class="button hollow expanded"><img src="//www.law-yamashita.com/wp-content/themes/ym-home/assets/img/header-tel.jpg" alt=""></a></p>
-			<p class="topheader--button"><a href="https://www.law-yamashita.com/contact" class="button hollow expanded" title="メールで相談予約" target="_blank"><?php NID_SVG::icon( 'mail', array( 'class' => 'button--icon' ), 'メール' ); ?>メールで相談予約</a></p>
-			<p class="topheader--button button-group expanded stacked-for-small"><a href="#front--flow" class="button hollow expanded" title="解決の流れ">解決の流れ</a><a href="#front--cases" class="button hollow expanded" title="こんなに違う！慰謝料などの賠償金">慰謝料などの差</a></p>
-			<p class="topheader--button"><a href="<?php echo home_url(), '/firm/access'; ?>" class="button hollow expanded" title="山下江法律事務所へのアクセス"><?php NID_SVG::icon( 'map', array( 'class' => 'button--icon' ), 'アクセス' ); ?>アクセス</a></p>
-		</div>
-	</div>
-</section>
+	</section>
