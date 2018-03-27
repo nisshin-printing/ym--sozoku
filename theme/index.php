88f313e3dfbb34ef126458a417db1648d610f92f
Kazuki Fukuda <nisshin@dtdsh.com>
2018-03-27 11:10:54 +0900

    重度障害追加

--- theme/index.php
+++ theme/index.php
@@ -7,20 +7,24 @@ if ( is_archive() || is_single() || is_home() ) {
 
 	get_header();
 ?>
-<main>
-	<header class="pageheader">
-		<div class="row">
-			<div class="column">
-				<h1 class="pageheader--title"><?php echo $title; ?></h1>
+	<main>
+		<header class="pageheader">
+			<div class="row">
+				<div class="column">
+					<h1 class="pageheader--title">
+						<?php echo $title; ?>
+					</h1>
+				</div>
 			</div>
-		</div>
-	</header>
-	<?php NID_Crumbs::crumbs(); ?>
-	<div class="row contents--wrap">
-		<div class="column large-3 sidebar--wrap"><?php get_sidebar(); ?></div>
-		<div class="sidenav--overlay js--sidenav--button"></div>
-		<div class="column contents">
-			<?php
+		</header>
+		<?php NID_Crumbs::crumbs(); ?>
+		<div class="row contents--wrap">
+			<div class="column large-3 sidebar--wrap">
+				<?php get_sidebar(); ?>
+			</div>
+			<div class="sidenav--overlay js--sidenav--button"></div>
+			<div class="column contents">
+				<?php
 				if ( have_posts() ) {
 					if ( is_page( 'service' ) ) {
 						get_template_part( 'templates/page--service' );
@@ -91,6 +95,12 @@ if ( is_archive() || is_single() || is_home() ) {
 							$show_cat = '119';
 							include locate_template( './templates/page--cases-voice.php' );
 
+						} else if ( is_page( 'cases/jyudo_syogai' ) ) {
+							$is_tag = true;
+							$is_type = 'cases';
+							$show_cat = '364';
+							include locate_template( './templates/page--cases-voice.php' );
+
 						} else if ( is_page( 'cases/other' ) ) {
 							$is_tag = true;
 							$is_type = 'cases';
@@ -140,9 +150,11 @@ if ( is_archive() || is_single() || is_home() ) {
 					echo '<h2 class="text-center">記事がありません。</h2>';
 				}
 			?>
+			</div>
+			<div class="column small-12">
+				<?php NID_Pagination::pagination(); ?>
+			</div>
 		</div>
-		<div class="column small-12"><?php NID_Pagination::pagination(); ?></div>
-	</div>
-</main>
-<?php
+	</main>
+	<?php
 	get_footer();
