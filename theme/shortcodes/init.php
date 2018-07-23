fcb0abed0bed7f0d7d9a41a94dd36f886caa2820
Kazuki Fukuda <nisshin@dtdsh.com>
2018-07-23 15:45:04 +0900

    基礎知識へ子ページ一覧を表示

--- theme/shortcodes/init.php
+++ theme/shortcodes/init.php
@@ -5,17 +5,16 @@
 add_shortcode( 'show_children', 'show_children_page' );
 function show_children_page() {
 	global $post;
-	$page_id = get_the_ID();
+	$page_id = $post->ID;
 	$args = array(
+		'post_type' => 'page',
 		'posts_per_page' => -1,
 		'post_parent' => $page_id,
-		'order' => 'ASC',
-		'orderby' => 'menu_order'
 	);
 	$posts = new WP_Query( $args );
 	$return = '<ul>';
 	if ( $posts->have_posts() ) : while ( $posts->have_posts() ) : $posts->the_post();
-	$return .= '<li><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></li>';
+	$return .= '<li><a href="' . get_the_permalink( $post->ID ) . '">' . get_the_title( $post->ID ) . '</a></li>';
 	endwhile; endif;
 	$return .= '</ul>';
 	return $return;
